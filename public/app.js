6835ed0f62d5bff471bdef5439d2a4e3a2824d75
“Foxshane997” <Foxshane997@gmail.com>
2024-04-03 21:46:21 -0700

    adding changes

--- public/app.js
+++ public/app.js
@@ -1,12 +1,9 @@
 //Note: You do NOT need to create an external JS file for this project despite any instructions you may see in Canvas. This file, app.js, is the only external JS file you need.
 
-
-
 // Create map
 // let map = L.map('map').setView([51.505, -0.09], 13);    /*from the quick start guide. */
 
-var myMap = L.map('map').setView([51.505, -0.09], 13);
-
+var myMap = L.map('map').setView([48.87007, 2.346453], 15);
 
 // Add openstreetmap tiles
 // L.tileLayer('https://.tile.openstreetmap.org///.png', {    /*from the quick start guide. */
@@ -18,8 +15,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(myMap);
-
-
 
 //Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
 
@@ -42,8 +37,27 @@ let polygon = L.polygon([
     [48.86834104280146, 2.330308418109664]
 ]).addTo(myMap);
 
+var popup = L.popup();
 
+/*popup on click */
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(myMap);
+}
+
+myMap.on('click', onMapClick);
 // Metro station markers
+// Metro station markers:
+const rS = L.marker([48.866200610611926, 2.352236247419453]).bindPopup('RÃ©aumur-SÃ©bastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196]).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup('Grands Boulevards')
+
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
 
 
 // Create red pin marker
